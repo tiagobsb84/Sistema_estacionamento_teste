9b9ab47823914ef874f599989060cd35454c8a90
Tiago <tiagobsb84@gmail.com>
2022-07-02 17:17:45 -0300

    Criando projeto Sistema estacionamento

---
+++ src/main/java/com/tiago/sistema/SistemaEstacionamentoApplication.java
@@ -0,0 +1,13 @@
+package com.tiago.sistema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEstacionamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaEstacionamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiago/sistema/SistemaEstacionamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiago.sistema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEstacionamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
